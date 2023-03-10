21cb6a31b1401c3276c687ea00cbf0966ec55ab0
QuanVo <quanb1401083tggdev@gmail.com>
2023-03-10 16:44:59 +0700

    Commit update giao diện player

--- lib/screen/player_screen.dart
+++ lib/screen/player_screen.dart
@@ -15,31 +15,38 @@ class _PlayerScreenState extends State<PlayerScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 100,
       width: MediaQuery.of(context).size.width,
-      color: Colors.grey[200],
-      child: CupertinoSlidingSegmentedControl(
-        padding: const EdgeInsets.all(10),
-        groupValue: groupValue,
-        thumbColor: CupertinoColors.activeGreen,
-        onValueChanged: (int? groupValue) {
-          setState(() {
-            this.groupValue = groupValue;
-          });
-        },
-        children: {
-          0: buildSegment('Player 1', 'assets/siunhanhuong.jpeg'),
-          1: buildSegment('Player 2', 'assets/siunhanvang.jpeg'),
-        },
-
-      )
+      alignment: Alignment.center,
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Container(
+            height: 100,
+            color: Colors.grey[200],
+            child: CupertinoSlidingSegmentedControl(
+              padding: const EdgeInsets.all(10),
+              groupValue: groupValue,
+              thumbColor: CupertinoColors.activeGreen,
+              onValueChanged: (int? groupValue) {
+                setState(() {
+                  this.groupValue = groupValue;
+                });
+              },
+              children: {
+                0: buildSegment('Player 1', 'assets/siunhanhuong.jpeg'),
+                1: buildSegment('Player 2', 'assets/siunhanvang.jpeg'),
+              },
+            )),
+      ),
     );
   }
 
   Widget buildSegment(String text, String avatarPath) {
     return Row(
       children: [
-        const SizedBox(width: 20,),
+        const SizedBox(
+          width: 20,
+        ),
         Container(
           alignment: Alignment.center,
           child: Row(
@@ -51,8 +58,7 @@ class _PlayerScreenState extends State<PlayerScreen> {
                   height: circleRadius,
                   decoration: BoxDecoration(
                     image: DecorationImage(
-                      image: AssetImage(
-                          avatarPath),
+                      image: AssetImage(avatarPath),
                       fit: BoxFit.fill,
                     ),
                     shape: BoxShape.circle,
@@ -71,21 +77,27 @@ class _PlayerScreenState extends State<PlayerScreen> {
                       child: Icon(Icons.person),
                     ),
                   )),
-              const SizedBox(width: 20,),
+              const SizedBox(
+                width: 20,
+              ),
               Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(text, style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 30,
-                      color: Colors.white
-                  ),),
-                  const SizedBox(height: 10,),
-                  const Text('Lv 20', style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.white
-                  ),)
+                  Text(
+                    text,
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30,
+                        color: Colors.white),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    'Lv 20',
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  )
                 ],
               )
             ],
